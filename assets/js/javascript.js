34ce2fe85628c1ca1a6c17a89264a99997a89a66
caroquintana <caro.quintana.n@gmail.com>
2017-04-24 21:16:11 -0400

    nuevos cambios definitivos en el html y javascript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,7 +1,7 @@
-var texto = document.getElementById("gradosFarenheit");
-
-function convertirCelsius(gradosFarenheit) {
-  gradosFarenheit = parseFloat(gradosFarenheit);
-  document.getElementById("gradosCelsius").innerHTML=(valorFarenheit-32)/1.8;
+function convertirCelsius() {
+	var grados = document.getElementById('farenheit').value;
+	var gradosNum = parseFloat(grados);
+	var resultado = ((gradosNum-32)/1.8).toFixed(2);
+	document.getElementById('gradosCelsius').value=resultado;
 };
 
